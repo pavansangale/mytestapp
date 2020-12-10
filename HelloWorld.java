bb32d3167767fa5a1b84dbf6239c82e56f37d4c6
pavan sangale <pavansangale04@gmail.com>
2020-12-10 13:15:34 +0530

    perodic build

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,5 +5,7 @@ public class HelloWorld{
         System.out.println("Hello world from app");
         System.out.println("Hello world from app");
         System.out.println("Hello world from app1");
+        System.out.println("Hello world from app1 when auto build ");
+
     }
 }
